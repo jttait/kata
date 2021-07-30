769e645a6507899dd24951e7ef6f307f1ca522b6
Jonathon Tait <jonathon.tait@tesco.com>
2021-07-30 20:34:36 +0100

    euler problem 11 in java

---
+++ projectEuler11/20210730_java/src/main/java/com/projecteuler/problem11/Problem11Application.java
@@ -0,0 +1,13 @@
+package com.projecteuler.problem11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Problem11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Problem11Application.class, args);
+	}
+
+}
