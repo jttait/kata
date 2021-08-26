682871f0c4d528c6e17a7b978c1999888821febc
Jonathon Tait <jonathon.tait@tesco.com>
2021-08-26 18:28:39 +0100

    project euler 25 in java

---
+++ ProjectEuler25/20210826_java/src/main/java/com/projecteuler/problem25/Problem25Application.java
@@ -0,0 +1,13 @@
+package com.projecteuler.problem25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Problem25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Problem25Application.class, args);
+	}
+
+}
