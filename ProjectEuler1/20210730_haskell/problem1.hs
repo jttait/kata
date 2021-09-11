cc95b4722ce2475a4cce413fc9b764a124d9a515
Jonathon Tait <jttait@gmail.com>
2021-09-11 11:29:42 +0100

    remove project euler

--- ProjectEuler1/20210730_haskell/problem1.hs
+++
@@ -1,2 +0,0 @@
-main :: IO ()
-main = print(sum(filter (\x -> x `mod` 3 == 0 || x `mod` 5 == 0) [1..999]))
--- ProjectEuler17/20210801_java/src/main/java/com/projecteuler/problem17/DigitConverter.java
+++
@@ -1,5 +0,0 @@
-package com.projecteuler.problem17;
-
-public interface DigitConverter {
-    public String convert(int number);
-}
