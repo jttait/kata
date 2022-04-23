a48f1cf9ac85cd51843069e8d0621f28e997ef7c
Jonathon Tait <jonathon.tait@tesco.com>
2022-04-23 08:59:42 +0100

    solve sudoku in java

---
+++ Sudoku/20220423_java/interview2/src/test/java/interview2/Interview2Test.java
@@ -0,0 +1,21 @@
+package interview2;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class Interview2Test {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
